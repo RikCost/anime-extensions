b1619754d216c639f5bf5ac555334a3ae748ff94
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2025-12-13 12:17:24 +0300

    Update domains for Hentaizm, TR Anime İzle, AsyaAnimeleri, Türk Anime
    (Kohi-den/extensions-source#1159)
    
    * Update AndroidManifest.xml
    
    * Update build.gradle
    
    * Update HentaiZM.kt
    
    * Update HentaiZMUrlActivity.kt
    
    * Update build.gradle
    
    * Update AndroidManifest.xml
    
    * Update TRAnimeIzle.kt
    
    * Update TRAnimeIzleUrlActivity.kt
    
    * Update build.gradle
    
    * Update AsyaAnimeleri.kt
    
    * Update build.gradle
    
    * Update TurkAnime.kt
    
    (cherry picked from commit 7d84012196ce6f13c400503a0a0eddb61db6be67)

--- src/tr/tranimeizle/src/eu/kanade/tachiyomi/animeextension/tr/tranimeizle/TRAnimeIzle.kt
+++ src/tr/tranimeizle/src/eu/kanade/tachiyomi/animeextension/tr/tranimeizle/TRAnimeIzle.kt
@@ -44,7 +44,7 @@ class TRAnimeIzle :
 
     override val name = "TR Anime Izle"
 
-    override val baseUrl = "https://www.tranimeizle.top"
+    override val baseUrl = "https://www.tranimeizle.io"
 
     override val lang = "tr"
 
