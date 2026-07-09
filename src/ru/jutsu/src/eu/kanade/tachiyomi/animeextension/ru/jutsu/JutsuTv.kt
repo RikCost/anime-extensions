01d7ec2681dc600accd7b1a1a7a751e7631de65d
RikCost <69048938+RikCost@users.noreply.github.com>
2026-07-09 14:34:50 +0300

    Clean up imports in JutsuTv.kt
    Removed unused imports from JutsuTv.kt

--- src/ru/jutsu/src/eu/kanade/tachiyomi/animeextension/ru/jutsu/JutsuTv.kt
+++ src/ru/jutsu/src/eu/kanade/tachiyomi/animeextension/ru/jutsu/JutsuTv.kt
@@ -25,9 +25,7 @@ import kotlinx.serialization.json.Json
 import okhttp3.FormBody
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
